96b7e065277621ced649597919bddec319dee65e
Vladimir <95054305+VlaSard@users.noreply.github.com>
2022-07-06 12:53:51 +0300

    Update day_in_month.py

--- function/day_in_month.py
+++ function/day_in_month.py
@@ -1,31 +1,37 @@
 def day_in_month(current_month: int, current_year: int) -> tuple[int, bool]:
-    """Функция определяет, сколько дней в месяце.
-    Работает для простого и високосного года.
+    """The function determines how many days are in a month, given the year type.
+    Works for prime year and leap year.
 
-    Входные данные: номер месяца и номер года.
-    На выходе получаем количество дней в месяце и признак високосного года.
-    Если год високосный вернет True, для невисокосного года - False.
+    The input data, month number and year number, must be of type int and have positive values.
+
+    The output is the number of days in a month and the type of year.
+
+    If the year is a leap year it will return True, for a non-leap year it will return False.
 
     Args:
-        current_month (int): номер месяца,
-        current_year (int): номер года.
+        current_month (int): month number.
+        current_year (int): month number.
 
     Returns:
-        tuple[int, bool]: количество дней в месяце и признак високосного года.
+        tuple[int, bool]: the number of days in a month and the type of year.
     """
 
-    # Проверка аргументов
+    # Arguments validation.
+    if isinstance(current_month, bool) or (isinstance(current_year, bool)):
+        raise TypeError("Unsupported data type. Arguments must be of type int.")
+
     if (isinstance(current_month, int) or isinstance(current_year, int)) is False:
-        raise TypeError("Не поддерживаемый тип данных.")
+        raise TypeError("Unsupported data type. Arguments must be of type int.")
 
     if (current_month or current_year) <= 0:
-        raise ValueError("Значение не может быть отрицательным.")
+        raise ValueError("The value cannot be negative.")
 
-    # Признак високосного года: 1 - год високосный, 0 - невисокосный.
+    # Determining the type of the year.
+    # 1 is a leap year, 0 is a non-leap year.
     leap_year = (1 - (current_year % 4 + 2) % (current_year % 4 + 1)) * (current_year % 100 + 2) % (
         current_year % 100 + 1) + (1 - (current_year % 400 + 2) % (current_year % 400 + 1))
 
-    # Количество дней в месяце.
+    # Calculation of the number of days in a month, taking into account the type of year.
     days_month = 28 + (current_month + (current_month // 8)) % 2 + 2 % current_month + (
         1 + leap_year) // current_month + (1 // current_month) - leap_year // current_month
 
